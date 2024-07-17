113305991febd3fabbc65a34cc107d81d7e00807
shubhangi kadam <spk192004@gmail.com>
2024-07-17 18:26:52 +0530

    final commit

---
+++ src/components/main.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <>
+      <div className="hero border-1 pb-3">
+        <div className="card bg-dark text-white border-0 mx-3" style={{height:"400px"}}>
+          <img
+            className="card-img img-fluid"
+            src="https://th.bing.com/th?id=OIP.g3xcOGZJ9u85ZzBzGU1yPgHaDT&w=350&h=156&c=8&rs=1&qlt=90&o=6&dpr=1.3&pid=3.1&rm=2"
+            alt="Card"
+            style={{height:"400px"}}
+          />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
